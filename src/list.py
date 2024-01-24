8c9c206dc27e16fed85852ad53d0507b0c91541b
galonso84 <e.gvpgal.cdo@telefonica.com>
2024-01-24 23:06:27 +0100

    add authorizationToken

--- src/list.py
+++ src/list.py
@@ -4,11 +4,28 @@ import todoList
 
 
 def list(event, context):
-    # fetch all todos from the database
-    result = todoList.get_items()
+    token = event['authorizationToken']
+    if token == 'allow':
+        print('authorized')
+        response = generatePolicy('user', 'Allow', event['methodArn'])
+    elif token == 'deny':
+        print('unauthorized')
+        response = generatePolicy('user', 'Deny', event['methodArn'])
+    elif token == 'unauthorized':
+        print('unauthorized')
+        raise Exception('Unauthorized') # Return a 401 Unauthorized response
+        return 'unauthorized'
+    try:
+        # fetch all todos from the database
+        result = todoList.get_items()
     # create a response
-    response = {
+        response = {
         "statusCode": 200,
         "body": json.dumps(result, cls=decimalencoder.DecimalEncoder)
-    }
-    return response
+        }
+        return response
+    except:
+        print('unauthorized')
+        return 'unauthorized' # Return a 500 response
+    
+    
