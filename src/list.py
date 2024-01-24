6f24440fe5c66e75c24cb3448e0bbca2ee1f82ab
galonso84 <e.gvpgal.cdo@telefonica.com>
2024-01-24 23:13:30 +0100

    .

--- src/list.py
+++ src/list.py
@@ -28,4 +28,23 @@ def list(event, context):
         print('unauthorized')
         return 'unauthorized' # Return a 500 response
     
-    
+def generatePolicy(principalId, effect, resource):
+        authResponse = {}
+        authResponse['principalId'] = principalId
+        if (effect and resource):
+            policyDocument = {}
+            policyDocument['Version'] = '2012-10-17'
+            policyDocument['Statement'] = [];
+            statementOne = {}
+            statementOne['Action'] = 'execute-api:Invoke'
+            statementOne['Effect'] = effect
+            statementOne['Resource'] = resource
+            policyDocument['Statement'] = [statementOne]
+            authResponse['policyDocument'] = policyDocument
+        authResponse['context'] = {
+            "stringKey": "stringval",
+            "numberKey": 123,
+            "booleanKey": True
+        }
+        authResponse_JSON = json.dumps(authResponse)
+        return authResponse_JSON
