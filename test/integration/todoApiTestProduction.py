50676167cc92060bf68846891bdcba9d26fcdc7f
galonso84 <e.gvpgal.cdo@telefonica.com>
2024-02-04 20:00:07 +0100

    add test_api_updatetodo

--- test/integration/todoApiTestProduction.py
+++ test/integration/todoApiTestProduction.py
@@ -87,4 +87,56 @@ class TestApi(unittest.TestCase):
             response.status_code, 200, "Error en la petición API a {url}"
         )
         print('End - integration test Get TODO')
-       
+       def test_api_updatetodo(self):
+        print('---------------------------------------')
+        print('Starting - integration test Update TODO')
+        #Add TODO
+        url = BASE_URL+"/todos"
+        data = {
+         "text": "Integration text example - Initial"
+        }
+        response = requests.post(url, data=json.dumps(data))
+        json_response = response.json()
+        print('Response Add todo: ' + json_response['body'])
+        jsonbody= json.loads(json_response['body'])
+        ID_TODO = jsonbody['id']
+        print ('ID todo:'+ID_TODO)
+        self.assertEqual(
+            response.status_code, 200, "Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            jsonbody['text'], "Integration text example - Initial", "Error en la petición API a {url}"
+        )
+        #Update TODO
+        url = BASE_URL+"/todos/" + ID_TODO
+        data = {
+         "text": "Integration text example - Modified",
+         "checked": "true"
+        }
+        response = requests.put(url, data=json.dumps(data))
+        json_response = response.json()
+        print('Response Update todo: ' + str(json_response))
+        #jsonbody= json.loads(json_response['body'])
+        self.assertEqual(
+            response.status_code, 200, "Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            json_response['text'], "Integration text example - Modified", "Error en la petición API a {url}"
+        )
+        #Test GET TODO
+        url = BASE_URL+"/todos/"+ID_TODO
+        response = requests.get(url)
+        json_response = response.json()
+        print('Response Get Todo: '+ str(json_response))
+        self.assertEqual(
+            response.status_code, 200, "Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            json_response['text'], "Integration text example - Modified", "Error en la petición API a {url}"
+        )
+        #Delete TODO to restore state
+        response = requests.delete(url)
+        self.assertEqual(
+            response.status_code, 200, "Error en la petición API a {url}"
+        )
+        print('End - integration test Update TODO')
